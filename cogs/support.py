cd3a541cd76791d4362cbd9fc97e597fd134eb00
Mark <miningmark48@gmail.com>
2021-06-07 21:19:18 -0400

    Added support commands, closes #15

---
+++ cogs/support.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+
+
+class Support(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+		self.kofi_link = "https://ko-fi.com/jensengillett"
+		self.paypal_link = "https://paypal.me/jensengillett"
+
+	@commands.command(aliases=["coffee", "buymeacoffee"])
+	async def kofi(self, ctx):
+		"""Support development on Ko-Fi!"""
+
+		await ctx.reply(self.kofi_link)
+
+	@commands.command(aliases=["pp"])
+	async def paypal(self, ctx):
+		"""Support development on PayPal!"""
+
+		await ctx.reply(self.paypal_link)
+
+
+def setup(bot):
+	bot.add_cog(Support(bot))
