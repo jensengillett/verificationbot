bbb26adc687961d8fea9acf38799ac64f0ed77a8
Mark <miningmark48@gmail.com>
2021-06-17 13:41:11 -0400

    Allow !email@email.com to run as email command
    #20

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,3 +1,5 @@
+import copy
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import errors as cmderr
@@ -18,8 +20,44 @@ class Errors(commands.Cog):
 			await ctx.channel.send("Missing required role to use this command!")
 		elif isinstance(exception, cmderr.MissingRequiredArgument):
 			await ctx.channel.send("Missing required arguments!")
+		elif isinstance(exception, cmderr.CommandNotFound):
+
+			# If the attempted command is a valid email, run the email command
+			email = ctx.message.content.replace(ctx.prefix, "")
+			if is_valid_email(email):
+				msg = copy.copy(ctx.message)
+				channel = ctx.channel
+				msg.channel = channel
+				msg.author = ctx.author
+				msg.content = f"{ctx.prefix}email {email}"
+				new_ctx = await self.bot.get_context(msg, cls=type(ctx))
+				await self.bot.invoke(new_ctx)
 		else:
 			print(exception)
+
+
+def is_valid_email(email: str):
+	print("Email ", email)
+
+	try:
+		dm = email.split('@')[1]  # split the string based on the @ symbol
+	except AttributeError as e:
+		print("DM EX\n", e)
+		return False
+
+	if not dm:
+		print("NOT DM")
+		return False
+
+	if len(email.split('@')[0]) > 64 or len(email.split('@')[1]) > 255:  # valid emails have 64char max before @, 255 max after
+		print("LEN")
+		return False
+
+	if set('+').intersection(email):  # to prevent people from making extra email addresses
+		print("+")
+		return False
+
+	return True
 
 
 def setup(bot):
