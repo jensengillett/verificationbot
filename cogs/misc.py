7deaf618d1777ddc7a58c12c2de532a5cc461056
Mark <miningmark48@gmail.com>
2021-06-17 20:50:30 -0400

    Merged support commands into single command, added other contributors
    #21

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,3 +1,5 @@
+from os import name
+import discord
 from discord.ext import commands
 
 
@@ -5,27 +7,37 @@ class Misc(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-		self.kofi_link = "https://ko-fi.com/jensengillett"
-		self.paypal_link = "https://paypal.me/jensengillett"
 		self.github_link = "https://github.com/jensengillett/verificationbot"
 
-	@commands.command(aliases=["coffee", "buymeacoffee"])
-	async def kofi(self, ctx):
-		"""Support development on Ko-Fi!"""
+	@commands.command(aliases=["coffee", "buymeacoffee", "paypal", "pp", "cashapp", "source"])
+	async def support(self, ctx):
+		"""Support development!"""
 
-		await ctx.reply(self.kofi_link)
+		embed = discord.Embed(title="Support", color=0x00ff00)
+		embed.description = f"[Repository]({self.github_link})"
+		embed.timestamp = ctx.message.created_at
 
-	@commands.command(aliases=["pp"])
-	async def paypal(self, ctx):
-		"""Support development on PayPal!"""
+		disc_jensen = """
+		Project Owner, Contributor, Maintainer
+		[Ko-fi](https://ko-fi.com/jensengillett)
+		[PayPal](https://paypal.me/jensengillett)
+		"""
+		embed.add_field(name="Jensen", value=disc_jensen, inline=False)
 
-		await ctx.reply(self.paypal_link)
+		desc_mark = """
+		Contributor, Maintainer, Initial Author
+		[Ko-fi](https://ko-fi.com/miningmark48)
+		[CashApp](https://cash.app/$MiningMark48)
+		"""
+		embed.add_field(name="Mark", value=desc_mark, inline=False)
 
-	@commands.command(aliases=["repo"])
-	async def source(self, ctx):
-		"""View the source code for the bot on Github!"""
+		# desc_aabuelazm = """
+		# Contributor, Maintainer
+		# LINKS TBD
+		# """
+		# embed.add_field(name="aabuelazm", value=desc_aabuelazm, inline=False)
 
-		await ctx.reply(self.github_link)
+		await ctx.reply(embed=embed)
 
 
 def setup(bot):
