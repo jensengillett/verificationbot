591fd5fede08438d67202df9cd12b0c48fe47f12
Jensen Gillett <jensengillett@uvic.ca>
2023-02-05 15:13:56 -0800

    -Now pops tokens from the active list once they're used successfully.

--- cogs/verification.py
+++ cogs/verification.py
@@ -234,8 +234,6 @@ class Verification(commands.Cog):
 			# Do the actual verification.
 			if self.token_list:
 				if self.token_list[ctx.author.id] == arg:
-					await ctx.send(f"{ctx.author.mention}, you've been verified!")
-
 					role = discord.utils.get(ctx.guild.roles, name=self.role)
 					if not role:
 						role = discord.utils.find(lambda r: str(r.id) == str(self.role), ctx.guild.roles)
@@ -245,6 +243,9 @@ class Verification(commands.Cog):
 						hashed = self.bot.hashing.hash(self.email_list[ctx.author.id])
 						file.write(f"{hashed}\n")
 						file.close()
+
+					self.token_list.pop(ctx.author.id)  # Remove the token from the active list.
+					await ctx.send(f"{ctx.author.mention}, you've been verified!")
 				else:
 					await ctx.send(f"Invalid token submitted, {ctx.author.mention}! Please submit the 4-digit token send to your email to verify.")
 					if self.verify_attempts:
